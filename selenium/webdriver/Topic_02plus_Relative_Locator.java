37c423eaa5e8e8b6191c490fb819960cf93c387d
hoanghaiphh <hoanghai.phh@gmail.com>
2023-08-22 18:18:44 +0700

    Demo Relative Locator

---
+++ selenium/webdriver/Topic_02plus_Relative_Locator.java
@@ -0,0 +1,76 @@
+package webdriver;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.locators.RelativeLocator;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+import java.awt.*;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+public class Topic_02plus_Relative_Locator {
+    WebDriver driver;
+    String projectPath = System.getProperty("user.dir");
+    String osName = System.getProperty("os.name");
+
+    @BeforeClass
+    public void beforeClass() {
+        if (osName.contains("Windows")) {
+            System.setProperty("webdriver.gecko.driver", projectPath + "\\browserDrivers\\geckodriver.exe");
+        } else {
+            System.setProperty("webdriver.gecko.driver", projectPath + "/browserDrivers/geckodriver");
+        }
+
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+        driver.manage().window().maximize();
+    }
+
+    @Test
+    public void TC_01_Relative() {
+        driver.get("https://demo.nopcommerce.com/login?returnUrl=%2Fregister");
+
+        // Login button
+        By loginButtonBy = By.cssSelector("button.login-button");
+        WebElement loginButtonElement = driver.findElement(By.cssSelector("button.login-button"));
+
+        // Remember me checkbox
+        By rememberMeBy = By.cssSelector("input#RememberMe");
+        WebElement rememberMeElement = driver.findElement(By.cssSelector("input#RememberMe"));
+
+        // Forgot password link
+        WebElement forgotPasswordElement = driver.findElement(By.cssSelector("span.forgot-password"));
+
+        // Password textbox
+        By passwordTextboxBy = By.cssSelector("input#Password");
+
+        // GUI (location/ position)
+        WebElement rememberMeTextElement = driver
+                .findElement(RelativeLocator.with(By.tagName("label"))
+                        .above(loginButtonBy)
+                        .toRightOf(rememberMeElement)
+                        .toLeftOf(forgotPasswordElement)
+                        .below(passwordTextboxBy)
+                        .near(forgotPasswordElement));
+        System.out.println(rememberMeTextElement.getText());
+
+        List<WebElement> alllinks = driver.findElements(RelativeLocator.with(By.tagName("a")));
+        System.out.println(alllinks.size());
+
+        By rememberMeTextBy = RelativeLocator.with(By.tagName("lable"))
+                .above(loginButtonElement)
+                .toRightOf(rememberMeBy);
+        System.out.println(rememberMeTextBy);
+
+    }
+
+    @AfterClass
+    public void afterClass() {
+        driver.quit();
+    }
+}
