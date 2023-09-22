2d2278e93a1d65983c7e34d294dd90d5d06ca793
hoanghaiphh <hoanghai.phh@gmail.com>
2023-09-22 09:03:02 +0700

    Update

--- selenium/webdriver/Topic_05_Selenium_New_Version.java
+++ selenium/webdriver/Topic_05_Selenium_New_Version.java
@@ -5,11 +5,13 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
+import java.time.Duration;
 import java.util.concurrent.TimeUnit;
 
 public class Topic_05_Selenium_New_Version {
@@ -37,6 +39,17 @@ public class Topic_05_Selenium_New_Version {
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.get("https://www.facebook.com/");
         driver.quit();
+    }
+
+    @Test
+    public void TC_04_Run_On_Safari() {
+        String osName = System.getProperty("os.name");
+        if (osName.contains("Mac")) {
+            driver = new SafariDriver();
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+            driver.get("https://www.facebook.com/");
+            driver.quit();
+        }
     }
 
 }
