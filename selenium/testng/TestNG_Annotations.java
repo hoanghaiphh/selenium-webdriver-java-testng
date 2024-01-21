3a0a17f0045e697443aa36f17180b3cc6e577ba2
Hoang Hai <hoanghai.phh@gmail.com>
2024-01-21 12:52:31 +0700

    [Topic 17] - TestNG Testing Framework

---
+++ selenium/testng/TestNG_Annotations.java
@@ -0,0 +1,67 @@
+package testng;
+
+import org.testng.annotations.*;
+
+public class TestNG_Annotations {
+
+    @BeforeClass
+    public void beforeClass() {
+        System.out.println("Before Class");
+    }
+
+    @AfterClass
+    public void afterClass() {
+        System.out.println("After Class");
+    }
+
+    @BeforeGroups
+    public void beforeGroup() {
+        System.out.println("Before Group");
+    }
+
+    @AfterGroups
+    public void afterGroup() {
+        System.out.println("After Group");
+    }
+
+    @BeforeMethod
+    public void beforeMethod() {
+        System.out.println("Before Method");
+    }
+
+    @AfterMethod
+    public void afterMethod() {
+        System.out.println("After Method");
+    }
+
+    @BeforeSuite
+    public void beforeSuite() {
+        System.out.println("Before Suite");
+    }
+
+    @AfterSuite
+    public void afterSuite() {
+        System.out.println("After Suite");
+    }
+
+    @BeforeTest
+    public void beforeTest() {
+        System.out.println("Before Test");
+    }
+
+    @AfterTest
+    public void afterTest() {
+        System.out.println("After Test");
+    }
+
+    @Test
+    public void TC_01() {
+        System.out.println("Test 01");
+    }
+
+    @Test
+    public void TC_02() {
+        System.out.println("Test 02");
+    }
+
+}
